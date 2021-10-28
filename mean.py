659927c1d5dc68a2c23f1da70ea71dbbe1c590c2
java-champ <71257597+java-champ@users.noreply.github.com>
2021-10-28 14:53:51 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,22 @@
+import csv
+
+with open("SOCR-HeightWeight.csv", newline='') as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+
+file_data.pop(0)
+new_data = []
+
+for i in range(len(file_data)):
+    n_number = file_data[i][1]
+    new_data.append(float(n_number))
+
+n=len(new_data)
+total=0
+
+for x in new_data:
+    total+=x
+
+mean = total/n
+
+print("Mean / Average is: " + str(mean))
